24b4022c0babaefea6b5d73862b8ca0b616eae6f
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-01-31 19:46:37 -0800

    Fixes

--- src/Terpz710/Claymores/Loader.php
+++ src/Terpz710/Claymores/Loader.php
@@ -7,24 +7,37 @@ namespace Terpz710\Claymores;
 use pocketmine\plugin\PluginBase;
 use pocketmine\item\Item;
 use pocketmine\player\Player;
-use pocketmine\math\Vector3;
+use pocketmine\block\Block;
 use pocketmine\block\VanillaBlocks;
+use pocketmine\item\VanillaItems;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
 
 use Terpz710\Claymores\Task\ExplosionTask;
 use Terpz710\Claymores\Event\EventListener;
+use Terpz710\Claymores\Command\ClaymoreCommand;
 
 class Loader extends PluginBase {
 
     public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer()->getCommandMap()->register("giveclaymore", new ClaymoreCommand($this));
     }
 
-    public function setClaymore(Player $player, Item $item) {
-        $position = $player->getPosition();
+    public function createClaymore(Player $player, int $amount): Item {
+        $claymoreItem = VanillaItems::BRICK();
+        $claymoreItem->getNamedTag()->setString("isClaymore", "");
+        $claymoreItem->setCustomName("Claymore");
+        $lore = ["Deadly explosive device"];
+        $claymoreItem->setLore($lore);
+        return $claymoreItem;
+    }
+
+
+    public function setClaymore(Player $player, Item $item, Block $block): void {
+        $position = $block->getPosition();
         $world = $player->getWorld();
-
-        $world->setBlock($position, VanillaBlocks::REDSTONE_BLOCK());
-
+        $world->setBlock($position, VanillaBlocks::REDSTONE());
         $this->getScheduler()->scheduleDelayedTask(new ExplosionTask($position, $world), 60);
     }
 }

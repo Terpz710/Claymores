2534d1337ef543619554f98bc6e36a8735d18d70
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-08-24 10:08:41 -0700

    Fixed a bug

--- src/Terpz710/Claymores/Command/ClaymoreCommand.php
+++ src/Terpz710/Claymores/Command/ClaymoreCommand.php
@@ -7,7 +7,6 @@ namespace Terpz710\Claymores\Command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\Server;
 
 use Terpz710\Claymores\Loader;
 
@@ -16,40 +15,42 @@ class ClaymoreCommand extends Command {
     private $plugin;
 
     public function __construct(Loader $plugin) {
-        parent::__construct("giveclaymore", "Give a claymore", "/giveclaymore [player]");
+        parent::__construct("giveclaymore", "Give a claymore", "/giveclaymore <player> <amount>");
         $this->plugin = $plugin;
         $this->setPermission("claymores.cmd");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if ($sender instanceof Player) {
-            if (count($args) !== 2) {
-                $sender->sendMessage("Usage:§e /giveclaymore [player]");
-                return true;
-            }
-
-            $targetPlayer = $this->plugin->getServer()->getPlayerExact($args[0]);
-            $amount = (int)$args[1];
-
-            if ($targetPlayer === null || !$targetPlayer->isOnline()) {
-                $sender->sendMessage("§l§c(!)§r§f Player not found or not online!");
-                return true;
-            }
-
-            if ($amount <= 0) {
-                $sender->sendMessage("§l§c(!)§r§f Amount must be a positive number!");
-                return true;
-            }
-
-            $claymoreItem = $this->plugin->createClaymore($sender, $amount);
-            $targetPlayer->getInventory()->addItem($claymoreItem);
-
-            $sender->sendMessage("§l§a(!)§r§f Given a claymore to §e{$targetPlayer->getName()}§f!");
-
+        if (!$this->testPermission($sender)) {
             return true;
-        } else {
+        }
+
+        if (!$sender instanceof Player) {
             $sender->sendMessage("This command can only be used by players");
             return false;
         }
+
+        if (count($args) !== 2) {
+            $sender->sendMessage("Usage: §e/giveclaymore <player> <amount>");
+            return true;
+        }
+
+        $targetPlayer = $this->plugin->getServer()->getPlayerExact($args[0]);
+        if ($targetPlayer === null || !$targetPlayer->isOnline()) {
+            $sender->sendMessage("§l§c(!)§r§f Player not found or not online!");
+            return true;
+        }
+
+        $amount = (int)$args[1];
+        if ($amount <= 0) {
+            $sender->sendMessage("§l§c(!)§r§f Amount must be a positive number!");
+            return true;
+        }
+
+        $claymoreItem = $this->plugin->createClaymore($targetPlayer, $amount);
+        $claymoreItem->setCount($amount);
+        $targetPlayer->getInventory()->addItem($claymoreItem);
+        $sender->sendMessage("§l§a(!)§r§f Given §e{$amount}§f claymore(s) to §e{$targetPlayer->getName()}§f!");
+        return true;
     }
 }
